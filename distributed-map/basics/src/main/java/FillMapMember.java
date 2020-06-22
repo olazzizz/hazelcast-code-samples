e0c729008dadf5e9289832f801d9b5da6a85262d
vagrant <vagrant@localhost.localdomain>
2020-06-22 09:13:11 +0000

    commented IMap

--- distributed-map/basics/src/main/java/FillMapMember.java
+++ distributed-map/basics/src/main/java/FillMapMember.java
@@ -1,6 +1,6 @@
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
+//import com.hazelcast.core.IMap;
 import com.hazelcast.core.Hazelcast;
 
 import java.util.Map;
