a703dce55bd95696b4e37cfc74f56df7f892b579
vagrant <vagrant@localhost.localdomain>
2020-06-22 09:25:36 +0000

    used the client

--- distributed-map/basics/src/main/java/PrintAllMember.java
+++ distributed-map/basics/src/main/java/PrintAllMember.java
@@ -1,12 +1,13 @@
-import com.hazelcast.core.Hazelcast;
+import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.Hazelcast;
 
 import java.util.Map;
 
 public class PrintAllMember {
 
     public static void main(String[] args) {
-        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        HazelcastInstance hz = HazelcastClient.newHazelcastClient();
         Map<String, String> map = hz.getMap("map");
         for (Map.Entry<String, String> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
